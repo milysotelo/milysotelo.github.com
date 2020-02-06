40c5fec03a8610850a943dd8f838a83c7466ae42
milysotelo <milysotelo@users.noreply.github.com>
2020-02-05 20:46:30 -0500

    Primera vez

---
+++ Boletines/javascript/main.js
@@ -0,0 +1,27 @@
+// obtener los elementos de la clase .close
+
+let links = document.querySelectorAll(".close");
+
+// recorrerlos
+
+links.forEach(function(link){
+
+	// agregar un evento click a cada uno de ellos
+	link.addEventListener("click",function(ev){
+		ev.preventDefault();
+		let content = document.querySelector(".content");
+
+		// quitarle las clases de animación que ya tiene
+		content.classList.remove("fadeInDown");
+		content.classList.remove("animated");
+
+		// agregar clases para animar su salida
+		content.classList.add("fadeOutUp");
+		content.classList.add("animated");
+
+		location.href = '/';
+
+		return false;
+	});
+});
+
